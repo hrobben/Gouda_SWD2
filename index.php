40f0e2cc2e783e42c156df21ea8154a983719ab5
sys72740hro <Hcbdag@635>
2022-09-29 14:10:28 +0200

    class

--- index.php
+++ index.php
@@ -1,21 +1,47 @@
 <?php
 
-$name = $_GET['name'];
-$title = 'les 2 php';
-echo "
-<!DOCTYPE html>
-<html lang=\"\">
-  <head>
-    <meta charset=\"utf-8\">
-    <title>$title</title>
-  </head>
-  <body>
-    <h1>Welkom op mijn site, groeten van Henry aan $name</h1>
-    <nav></nav>
-    <header></header>
-    <main></main>
-    <footer></footer>
-  </body>
-</html>
-";
- ?>
+class Render
+{
+    // variabelen declaratie
+    public $filename = 'web.html';
+    private $name = 'Hrobben';
+    public $title = 'dit is uit de class als object';
+
+    // method declaration
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function index()
+    {
+        //echo file_get_contents($this->filename);
+        echo "
+            <!DOCTYPE html>
+            <html lang=\"\">
+              <head>
+                <meta charset=\"utf-8\">
+                <title>$this->title</title>
+              </head>
+              <body>
+                <h1>Welkom op mijn site, groeten van Henry aan $this->name</h1>
+                <nav></nav>
+                <header></header>
+                <main></main>
+                <footer></footer>
+              </body>
+            </html>
+        ";
+    }
+}
+
+$site = new Render();
+echo $site->getName();
+$site->setName($_GET['name']);
+$site->title = 'les 2 php';
+
+$site->index();
+?>
